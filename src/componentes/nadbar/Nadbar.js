54aeada484a9cfa86185e737b95659f67a3e9af2
emanuel heredia <ema_359@hotmail.com>
2022-09-09 14:38:58 -0300

    configuracion aptitudes y ajuste responsivo

--- src/componentes/nadbar/Nadbar.js
+++ src/componentes/nadbar/Nadbar.js
@@ -1,17 +1,31 @@
 import "./nadbar.css";
 
 const Nadbar = ({ scrollHeight }) => {
+	const tamañoPAantalla = window.screen.width;
 	const toTheTop = () => {
 		window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
 	};
+	console.log(tamañoPAantalla);
 	const toTheAbout = () => {
-		window.scrollTo({ top: 800, left: 0, behavior: "smooth" });
+		window.scrollTo({
+			top: tamañoPAantalla < 450 ? 750 : 800,
+			left: 0,
+			behavior: "smooth",
+		});
 	};
 	const toTheProyects = () => {
-		window.scrollTo({ top: 1500, left: 0, behavior: "smooth" });
+		window.scrollTo({
+			top: tamañoPAantalla < 450 ? 1300 : 1500,
+			left: 0,
+			behavior: "smooth",
+		});
 	};
 	const toTheAptitudes = () => {
-		window.scrollTo({ top: 2350, left: 0, behavior: "smooth" });
+		window.scrollTo({
+			top: tamañoPAantalla < 450 ? 1800 : 2350,
+			left: 0,
+			behavior: "smooth",
+		});
 	};
 	const toTheContact = () => {
 		window.scrollTo({ top: 3500, left: 0, behavior: "smooth" });
